71b5ce71e6d97d1d8e98ce764e983402ecf89dc8
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-09-20 16:53:27 +0800

    Add fake apis
    will remove when backend is ready

--- src/api/api.js
+++ src/api/api.js
@@ -3,13 +3,57 @@ import axios from 'axios';
 const config = require('./config.json');
 
 function postBase64Image(base64Image) {
-  return axios.post(config.urlBase + config.pathSession, {
-    image: base64Image
-  });
+  return axios
+    .post(config.urlBase + config.pathSession, {
+      image: base64Image
+    })
+    .then(() => {
+      return {
+        data: {
+          mids: ['U1658131C']
+        }
+      };
+    });
 }
 
 function getLabsByTid(tid) {
   return axios.get(`${config.urlBase + config.pathLab}?instructors=${tid}`);
+}
+
+function getStudentsByLid(lid) {
+  console.log(`getStudentsByLid: ${lid}`);
+  return new Promise(res =>
+    res([
+      { mid: 'U1622139C', name: 'Qiu Haoze' },
+      {
+        mid: 'U1658131C',
+        name: 'John Doe Alamak Name Very Damn Freaking Long'
+      }
+    ])
+  );
+}
+
+function startSession(lid) {
+  console.log(`startSession: ${lid}`);
+  return new Promise(res =>
+    res({
+      data: {
+        sid: '1',
+        students: [
+          { mid: 'U1622139C', name: 'Qiu Haoze' },
+          {
+            mid: 'U1658131C',
+            name: 'John Doe Alamak Name Very Damn Freaking Long'
+          }
+        ]
+      }
+    })
+  );
+}
+
+function endSession(sid) {
+  console.log(`endSession: ${sid}`);
+  return new Promise(res => res({}));
 }
 
 function getToken(username, password) {
@@ -29,5 +73,8 @@ export default {
   postBase64Image,
   getToken,
   getLabsByTid,
-  verifyToken
+  getStudentsByLid,
+  verifyToken,
+  startSession,
+  endSession
 };
