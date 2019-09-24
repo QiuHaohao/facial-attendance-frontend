ccf12a468fc8f9461ed82294543c435fa3c5bc86
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-09-24 14:54:29 +0800

    Add warning and notif when recording paused/resumed
    Add second argument to path change effect
    
    
    -

--- src/components/MainView/Content/Content.js
+++ src/components/MainView/Content/Content.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { withRouter, Route } from 'react-router-dom';
+import { message } from 'antd';
 
 import { useUser } from '../../../hooks/userHook';
+import { useSession } from '../../../hooks/sessionHook';
 
 import ConditionalRedirectRoute from '../../commons/ConditionalRedirectRoute';
 import PageSession from '../../pages/PageSession';
 import PageLabs from '../../pages/PageLabs';
 import PageSignIn from '../../pages/PageSignIn';
 
-function Content() {
+function Content(props) {
   const user = useUser();
+  const session = useSession();
+
+  const [prevPathname, setPrevPathname] = useState(null);
+
+  useEffect(() => {
+    const currentPathname = props.location.pathname;
+    const handlePathnameChange = (prev, cur) => {
+      // from /session/ongoing to anywhere else, and session is going on
+      if (prev === '/session/ongoing' && session.isOnGoing) {
+        message.destroy();
+        message.warning(
+          'Recording paused. \n Go back to session page to resume'
+        );
+      }
+      // from anywhere else to /session ,  and session is going on
+      if (
+        cur.startsWith('/session') &&
+        prev !== null &&
+        !prev.startsWith('/session') &&
+        session.isOnGoing
+      ) {
+        message.destroy();
+        message.success('Recording resumed.');
+      }
+    };
+    if (prevPathname !== currentPathname) {
+      handlePathnameChange(prevPathname, currentPathname);
+      setPrevPathname(currentPathname);
+    }
+  }, [props.location.pathname, prevPathname]);
 
   const renderContent = () => (
     <React.Fragment>
@@ -37,4 +70,10 @@ function Content() {
   );
 }
 
-export default Content;
+Content.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  })
+};
+
+export default withRouter(Content);
