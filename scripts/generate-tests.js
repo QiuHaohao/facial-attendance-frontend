e7841d9df8f17b2580452783317af9efc6e98b4f
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-10-19 12:33:35 +0800

    Add test generation script

---
+++ scripts/generate-tests.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
+const PATH_COMPONENTS = './src/components';
+
+const test = folderName => `
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ${folderName} from './${folderName}';
+
+// eslint-disable-next-line no-undef
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<${folderName} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+`;
+
+// Add test file if
+// [NAME_FOLDER].js exists
+// index.js exists
+// [NAME_FOLDER].test.js doesn't exist
+
+function getFolderName(pathFolder) {
+  return pathFolder.split('/').slice(-1)[0];
+}
+
+function addTestFile(pathFolder) {
+  const folderName = getFolderName(pathFolder);
+  const testString = test(folderName);
+  const pathTestFile = path.join(pathFolder, `${folderName}.test.js`);
+  fs.writeFileSync(pathTestFile, testString);
+}
+
+function shouldAddTestFile(pathFolder) {
+  const folderName = getFolderName(pathFolder);
+  return (
+    fs.existsSync(path.join(pathFolder, 'index.js')) &&
+    fs.existsSync(path.join(pathFolder, `${folderName}.js`)) &&
+    !fs.existsSync(path.join(pathFolder, `${folderName}.test.js`))
+  );
+}
+
+function addIfShouldAddTestFile(pathFolder) {
+  if (shouldAddTestFile(pathFolder)) {
+    addTestFile(pathFolder);
+  }
+}
+
+function execute(pathFolder) {
+  const subFolders = fs
+    .readdirSync(pathFolder)
+    .map(filename => path.join(pathFolder, filename))
+    .filter(p => fs.lstatSync(p).isDirectory());
+  subFolders.forEach(addIfShouldAddTestFile);
+  subFolders.forEach(execute);
+}
+
+execute(PATH_COMPONENTS);
