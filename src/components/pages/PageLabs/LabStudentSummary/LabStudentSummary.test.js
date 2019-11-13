ab6dca9c8dc34f61d2847b87a6495c2bbf2e5711
PLC0114 <p1805924478@gmail.com>
2019-11-13 21:48:36 +0800

    Update LabStudentSummary.test.js

--- src/components/pages/PageLabs/LabStudentSummary/LabStudentSummary.test.js
+++ src/components/pages/PageLabs/LabStudentSummary/LabStudentSummary.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import LabStudentSummary from './LabStudentSummary';
 import api from '../../../../api/api';
 
@@ -15,10 +15,37 @@ describe('LabStudentSummary component', () => {
     // eslint-disable-next-line no-undef
     jest.spyOn(api, 'getStudentByMid').mockImplementation(() =>
       Promise.resolve({
-        json: () => {}
+        json: () => {
+          return {
+            name: 'XX',
+            email: '000@gmail.com',
+            sessions: [
+              {
+                sid: 1,
+                attendance: 'A',
+                remark: ''
+              }
+            ]
+          };
+        }
       })
     );
 
     mount(<LabStudentSummary match={match} />);
   });
+
+  // // eslint-disable-next-line no-undef
+  // test('click save', () => {
+  //   const match = {
+  //     params: {
+  //       id: 'U17872837D'
+  //     }
+  //   };
+  //   const record = {
+  //     sid: 5
+  //   };
+  //   // eslint-disable-next-line no-undef
+  //   const component = shallow(<LabStudentSummary match={match} />);
+  //   component.find('')
+  // });
 });
